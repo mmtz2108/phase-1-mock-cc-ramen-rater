b4a3567a8d146f5c942f44ab6aff4abd9d8e6539
Marcos Martinez <mmtz2108@gmail.com>
2022-04-13 10:49:36 -0500

    commit tree 2ee8bfea7da53efc3cf0734b40cb8df9eb32f0fb

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-// write your code here
+fetch('http://localhost:3000/ramens')
+.then(res => res.json)
+.then(ramens => console.log(ramens)); 
+
+
+
+const displayInfo = document.querySelectorAll('img').forEach((item) =>
+    item.addEventListener('click', (e) => {
+  //Here we want the ramen img to display and display its info
+    e.target.ramens.id    
+    }));
